93f2ad46648ed2fd80740842d5e634dded25caff
charudatta <152109007c@gmail.com>
2024-07-04 13:04:42 +0530

    added support for .xcf

--- move_T2506.py
+++ move_T2506.py
@@ -72,11 +72,11 @@ if __name__ == "__main__":
     cdo = cleanup_directory()
     user_name = os.getlogin()
     cdo.source_directory = f"C:/Users/{user_name}/Downloads"
-    # cdo.archive_directory = f"C:/Users/{user_name}/Downloads/Archives"
+    cdo.archive_directory = f"C:/Users/{user_name}/Downloads/Archives"
     while True:
         cdo.get_dest_dir()
         cdo.move_files()
-        val = input("Enter source directory continue or C to exit")
+        val = input("Enter source directory continue or C to exit: ")
         if val == "c" or val == "C":
             break
         else:
