9465ffefde2c99b3bc91dccf01865fb77f21c13d
charudatta <152109007c@gmail.com>
2024-07-16 18:30:51 +0530

    modified main file

---
+++ __main__.py
@@ -0,0 +1,14 @@
+from src.move_T2506 import cleanup_directory
+import os
+cdo = cleanup_directory()
+user_name = os.getlogin()
+cdo.source_directory = f"C:/Users/{user_name}/Downloads"
+cdo.archive_directory = f"C:/Users/{user_name}/Downloads/Archives"
+while True:
+    cdo.get_dest_dir()
+    cdo.move_files()
+    val = input("Enter source directory continue or C to exit: ")
+    if val == "c" or val == "C":
+        break
+    else:
+        cdo.source_directory = val
